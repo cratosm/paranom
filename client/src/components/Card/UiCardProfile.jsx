c3ed8cce220c7678366d288e03a4f337ddbba1dd
matthias <matthias.virassamy@epitech.eu>
2023-12-11 09:40:18 +0100

    sandbox for UiHeader2: add new card for the profile

---
+++ client/src/components/Card/UiCardProfile.jsx
@@ -0,0 +1,30 @@
+import Lelouch from "../../assets/Image/Piccolo H.png"
+import logo from "../../assets/Logo/fleur.png"
+
+export const UiCardProfile = () => {
+    return (
+        <div className="relative flex w-72 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div
+                className="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+                <img src={Lelouch} alt="profile-picture"/>
+            </div>
+            <div className="p-6 text-center">
+                <img src={logo} alt="profile-picture" className="absolute w-20 left-2/3"/>
+                <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                    MParanom
+                </h4>
+                <p className="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
+                    Co-Founder
+                </p>
+            </div>
+            <div className="flex justify-center gap-7 p-6 pt-2">
+                <a
+                    href="#instagram"
+                    className="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
+                >
+                    <i className="fab fa-instagram" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    )
+}
--- client/src/components/SubHeader/UiSubHeader2.jsx
+++ client/src/components/SubHeader/UiSubHeader2.jsx
@@ -1,6 +1,7 @@
 import {UiInput} from "../Input/UiInput";
 import MParanom from "../../assets/Image/MParanom.png"
 import Amber from "../../assets/Image/Amber H.png"
+import {UiCardProfile} from "../Card/UiCardProfile";
 
 export const UiSubHeader2 = ({title, titleSecond, subTitle, subTitleSecond, input, image, imgClassName}) => {
     return (
@@ -38,37 +39,7 @@ export const UiSubHeader2 = ({title, titleSecond, subTitle, subTitleSecond, inpu
                     </div>
                 </div>
                 <div className="bg-black md:w-1/2 flex items-center justify-center pt-10 md:pt-0">
-                    <div
-                        className="flex-shrink-0 m-6 relative overflow-hidden bg-orange-500 rounded-lg max-w-xs shadow-lg">
-                        <div className="relative pt-10 px-10 flex items-center justify-center">
-                            <div className="block absolute w-48 h-48 bottom-0 left-28 -mb-24 ml-3 rounded-lg"
-                                 style={{
-                                     background: 'white',
-                                     transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
-                                     opacity: 0.2
-                                 }}
-                            ></div>
-                            <div className="block absolute w-48 h-48 top-0 right-28 rounded-lg"
-                                 style={{
-                                     background: 'white',
-                                     transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
-                                     opacity: 0.2
-                                 }}
-                            ></div>
-                            <img className="relative w-40"
-                                 src={MParanom}
-                                 alt=""/>
-                        </div>
-                        <div className="relative text-white px-6 pb-6 mt-6">
-                            <span className="block opacity-75 -mb-1">Indoor</span>
-                            <div className="flex justify-between">
-                                <span className="block font-semibold text-xl">Peace Lily</span>
-                                <span
-                                    className="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <img src={image} alt="ParanomBoomerang" className={imgClassName} />
+                    <UiCardProfile />
                 </div>
             </div>
         </div>
